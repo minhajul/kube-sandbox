89dae0a36fab5da1b699cf001ebb2e34c569351d
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-07-19 14:32:08 +0600

    Fixed frontend issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,11 +6,11 @@ const nextConfig = {
     return [
       {
         source: '/api/auth/:path*',
-        destination: `${process.env.AUTH_SERVICE_URL || 'http://auth-service-svc.default.svc.cluster.local:3001'}/:path*`,
+        destination: `${process.env.AUTH_SERVICE_URL || 'http://apps-kube-sandbox-auth:3001'}/:path*`,
       },
       {
         source: '/api/profile/:path*',
-        destination: `${process.env.PROFILE_SERVICE_URL || 'http://profile-service-svc.default.svc.cluster.local:3002'}/:path*`,
+        destination: `${process.env.PROFILE_SERVICE_URL || 'http://apps-kube-sandbox-profile:3002'}/:path*`,
       },
     ];
   },
